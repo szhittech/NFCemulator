83c754065d5876efe0967ee3cf6396113a35cdbf
Wouter Peetermans <wouter@wouterpeetermans.com>
2016-12-12 10:09:21 +0100

    initial commit

---
+++ app/src/main/java/com/wouterpeetermans/nfcemulator/mainScreen.java
@@ -0,0 +1,13 @@
+package com.wouterpeetermans.nfcemulator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
